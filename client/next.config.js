376746d6b33d315875755d659b1d9593580dd189
Soumik <soumik.ahammed.9@gmail.com>
2023-03-23 13:25:50 +0600

    backend url domain added

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'render.com'],
+    domains: ['localhost', 'render.com', 'rider-app.onrender.com'],
   },
 };
 
